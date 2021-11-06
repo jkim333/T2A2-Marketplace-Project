96dba6c398769cd141c65952e4de4022622a44dd
jkim333 <kimjihyung3@gmail.com>
2021-11-06 13:48:02 +1100

    Add devise gem.

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  validates :username, presence: { message: "can't be blank" },
+            uniqueness: true, length: { maximum: 20 },
+            format: { with: /\A[\S]+\z/,
+              message: "can't have whitespace characters" }
+end
