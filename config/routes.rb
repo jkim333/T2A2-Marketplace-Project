729cdbec7cb38c896513b19be44457af32363777
jkim333 <kimjihyung3@gmail.com>
2021-11-09 17:32:14 +1100

    Create home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'users/registrations' }
-  root to:'products#index'
+  root to:'static_pages#home'
 
+  get "products", to: "products#index"
   get "/products/:slug", to: "products#show"
 
   get "/profile/purchase", to: "profiles#purchase"
